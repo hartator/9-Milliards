9bce57749405491404230c4f98ddf7a5b420da7d
hartator <hartator@gmail.com>
2015-05-25 15:09:46 -0500

    Connect Server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,11 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '188.166.72.229', user: 'deploy', roles: %w{web app}
+server '188.166.72.229', user: 'root', roles: %w{web app}
+
+set :ssh_options, {
+  forward_agent: true,
+}
 
 # server-based syntax
 # ======================
