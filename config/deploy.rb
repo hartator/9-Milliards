082bacb82e0bbcf048b55cabe5f2a8013b0f84e1
Hartator <hartator@gmail.com>
2015-06-14 23:09:48 -0500

    Fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :repo_url, 'https://github.com/hartator/9-Milliards.git'
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp vendor/bundle public/system public/assets}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
