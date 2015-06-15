1317c4a27fc85ddeb0fd040fbf2b24e1bde09e57
Hartator <hartator@gmail.com>
2015-06-14 22:31:45 -0500

    Install React Rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require home
 //= require_tree .
